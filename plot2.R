9106d491f2ab8f600e4243191115654fb06de179
Zhenis Beisekov <zhenis.beisekov@gmail.com>
2019-11-17 22:39:53 +0100

    Second plot is ready

---
+++ plot2.R
@@ -0,0 +1,9 @@
+library(lubridate)
+
+plot.plot2 <- function(epc) {
+    png("plot2.png")
+    with(epc, plot(Timestamp, Global_active_power, type = "n",
+                   xlab = "", ylab = "Global active power (kilowatts)"))
+    with(epc, lines(Timestamp, Global_active_power))
+    dev.off()
+}
