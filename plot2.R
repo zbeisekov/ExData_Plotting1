cfd37ed0ffb837c1e8c1f6722958d952e72a402e
Zhenis Beisekov <zhenis.beisekov@gmail.com>
2019-11-17 23:00:54 +0100

    Refactored plot function to not plot to the file

--- plot2.R
+++ plot2.R
@@ -1,7 +1,5 @@
 plot.plot2 <- function(epc) {
-    png("plot2.png")
     with(epc, plot(Timestamp, Global_active_power, type = "n",
                    xlab = "", ylab = "Global active power (kilowatts)"))
     with(epc, lines(Timestamp, Global_active_power))
-    dev.off()
 }
--- plot3.R
+++ plot3.R
@@ -1,5 +1,4 @@
 plot.plot3 <- function(epc) {
-    png("plot3.png")
     with(epc, plot(Timestamp, Sub_metering_1, type = "n",
                    xlab = "", ylab = "Energy sub metering"))
     with(epc, lines(Timestamp, Sub_metering_1, col = "black"))
@@ -9,5 +8,4 @@ plot.plot3 <- function(epc) {
            legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
            col = c("black", "red", "blue"),
            lty = 1)
-    dev.off()
 }
