1415ea412da1e57969defbe0aab21268dcdeb9ea
Zhenis Beisekov <zhenis.beisekov@gmail.com>
2019-11-17 22:49:02 +0100

    Plot3 has been finished

---
+++ plot3.R
@@ -0,0 +1,13 @@
+plot.plot3 <- function(epc) {
+    png("plot3.png")
+    with(epc, plot(Timestamp, Sub_metering_1, type = "n",
+                   xlab = "", ylab = "Energy sub metering"))
+    with(epc, lines(Timestamp, Sub_metering_1, col = "black"))
+    with(epc, lines(Timestamp, Sub_metering_2, col = "red"))
+    with(epc, lines(Timestamp, Sub_metering_3, col = "blue"))
+    legend("topright", 
+           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+           col = c("black", "red", "blue"),
+           lty = 1)
+    dev.off()
+}
